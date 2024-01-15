f6d6850e79ba5edf9170d0054cafd46e631eae5a
James Cranch <cranch@cantab.net>
2024-01-15 11:29:23 +0000

    Homogeneous coordinates

--- modern-map-merge/src-spacetrees/Data/SpaceTree/Coords.hs
+++ modern-map-merge/src-spacetrees/Data/SpaceTree/Coords.hs
@@ -115,6 +115,35 @@ instance (Coordinate b p i,
   greatestPoint (Rectangle u v) = (greatestPoint u, greatestPoint v)
 
 
+-- | The common case of a homogeneous rectangle
+data Rectangle' b p i = Rectangle' {
+  rectanglex' :: b,
+  rectangley' :: b
+} deriving (Eq, Ord, Read, Show)
+
+instance (Semigroup b) => Semigroup (Rectangle' b p i) where
+  Rectangle' x1 y1 <> Rectangle' x2 y2 = Rectangle' (x1 <> x2) (y1 <> y2)
+
+instance (Coordinate b p i)
+       => Coordinate (Rectangle' b p i) (p,p) (i,i) where
+
+  narrow (Rectangle' u v) (p, q) = let
+    (i, u') = narrow u p
+    (j, v') = narrow v q
+    in ((i, j), Rectangle' u' v')
+
+  subbox (Rectangle' u v) (i, j) = Rectangle' (subbox u i) (subbox v j)
+
+  pointBox (p,q) = Rectangle' (pointBox p) (pointBox q)
+
+  disjoint (Rectangle' u v) (Rectangle' u' v') = disjoint u u' || disjoint v v'
+
+  isSubset (Rectangle' u v) (Rectangle' u' v') = isSubset u u' && isSubset v v'
+
+  leastPoint    (Rectangle' u v) = (leastPoint u, leastPoint v)
+  greatestPoint (Rectangle' u v) = (greatestPoint u, greatestPoint v)
+
+
 data Cuboid b c d p q r i j k = Cuboid {
   cuboidx :: b,
   cuboidy :: c,
@@ -146,3 +175,32 @@ instance (Coordinate b p i,
   leastPoint    (Cuboid u v w) = (leastPoint u, leastPoint v, leastPoint w)
   greatestPoint (Cuboid u v w) = (greatestPoint u, greatestPoint v, greatestPoint w)
 
+
+data Cuboid' b p i = Cuboid' {
+  cuboidx' :: b,
+  cuboidy' :: b,
+  cuboidz' :: b
+} deriving (Eq, Ord, Read, Show)
+
+instance (Semigroup b) => Semigroup (Cuboid' b p i) where
+  Cuboid' x1 y1 z1 <> Cuboid' x2 y2 z2 = Cuboid' (x1 <> x2) (y1 <> y2) (z1 <> z2)
+
+instance (Coordinate b p i)
+       => Coordinate (Cuboid' b p i) (p,p,p) (i,i,i) where
+
+  narrow (Cuboid' u v w) (p, q, r) = let
+    (i, u') = narrow u p
+    (j, v') = narrow v q
+    (k, w') = narrow w r
+    in ((i, j, k), Cuboid' u' v' w')
+
+  subbox (Cuboid' u v w) (i, j, k) = Cuboid' (subbox u i) (subbox v j) (subbox w k)
+
+  pointBox (p,q,r) = Cuboid' (pointBox p) (pointBox q) (pointBox r)
+
+  disjoint (Cuboid' u v w) (Cuboid' u' v' w') = disjoint u u' || disjoint v v' || disjoint w w'
+
+  isSubset (Cuboid' u v w) (Cuboid' u' v' w') = isSubset u u' && isSubset v v' && isSubset w w'
+
+  leastPoint    (Cuboid' u v w) = (leastPoint u, leastPoint v, leastPoint w)
+  greatestPoint (Cuboid' u v w) = (greatestPoint u, greatestPoint v, greatestPoint w)
