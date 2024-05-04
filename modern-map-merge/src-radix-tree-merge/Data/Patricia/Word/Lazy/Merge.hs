66bc4015832cfd5ac4ab8177a93ba31956d7acfb
James Cranch <cranch@cantab.net>
2024-05-04 23:41:40 +0100

    Making a mess of merge

--- modern-map-merge/src-radix-tree-merge/Data/Patricia/Word/Lazy/Merge.hs
+++ modern-map-merge/src-radix-tree-merge/Data/Patricia/Word/Lazy/Merge.hs
@@ -1,18 +1,26 @@
 {-# LANGUAGE
-      MultiParamTypeClasses
+      BangPatterns,
+      ExistentialQuantification,
+      GADTs,
+      MultiParamTypeClasses,
+      RankNTypes,
+      UnboxedTuples
   #-}
 
 module Data.Patricia.Word.Lazy.Merge where
 
+import Data.Bits ((.|.))
 import Data.Foldable.WithIndex
 import Data.Functor.WithIndex
 import Data.Traversable.WithIndex
 import Data.Maplike
+import Data.MergeTactics
 import Data.Patricia.Word.Lazy.Unsafe (Patricia(..), Prefix)
 import qualified Data.Patricia.Word.Lazy as P
+import qualified Data.Patricia.Word.Lazy.Unsafe as P
 import Witherable
 
--- This function is important but inaccessible :-(
+-- This functionality is important but inaccessible :-(
 
 {-# INLINE rebin #-}
 -- | Reconstruct a 'Bin' knowing that either of the sides may now be a 'Nil'.
@@ -21,6 +29,56 @@ rebin _ Nil r   = r
 rebin _ l   Nil = l
 rebin p l   r   = Bin p l r
 
+{-# INLINE rebinL #-}
+-- | Reconstruct a 'Bin' knowing that the left side may now be a 'Nil'.
+rebinL :: Prefix -> Patricia a -> Patricia a -> Patricia a
+rebinL p l r =
+  case l of
+    Nil -> r
+    _   -> Bin p l r
+
+{-# INLINE rebinR #-}
+-- | Reconstruct a 'Bin' knowing that the right side may now be a 'Nil'.
+rebinR :: Prefix -> Patricia a -> Patricia a -> Patricia a
+rebinR p l r =
+  case r of
+    Nil -> l
+    _   -> Bin p l r
+
+
+{-# INLINE safeJoin #-}
+-- | Knowing that the prefixes of two trees disagree, construct a 'Bin'.
+safeJoin :: Prefix -> Patricia a -> Prefix -> Patricia a -> Patricia a
+safeJoin _  Nil _  t1  = t1
+safeJoin _  t0  _  Nil = t0
+safeJoin p0 t0  p1 t1  = join p0 t0 p1 t1
+
+{-# INLINE join #-}
+-- | Knowing that the prefixes of two non-'Nil' trees disagree, construct a 'Bin'.
+join :: Prefix -> Patricia a -> Prefix -> Patricia a -> Patricia a
+join p0 t0 p1 t1 =
+  let m = P.branchingBit p0 p1
+      p = P.mask p0 m .|. m
+  in if P.zeroBit p0 m
+       then Bin p t0 t1
+       else Bin p t1 t0
+
+type UBin a = (# Prefix, Patricia a, Patricia a #)
+
+type UTip a = (# Word, a #)
+
+-- | Merge side.
+data S a b x y where
+  L :: S x y x y
+  R :: S y x x y
+
+-- | The other merge side.
+other :: S a b x y -> (# S b a x y #)
+other L = (# R #)
+other R = (# L #)
+
+
+
 instance Filterable Patricia where
   mapMaybe = P.mapMaybe
 
@@ -28,11 +86,8 @@ instance Witherable Patricia where
   wither f = let
     go t = case t of
       Bin p l r -> liftA2 (rebin p) (go l) (go r)
-      Tip k a -> let
-        g (Just b) = Tip k b
-        g Nothing  = Nil
-        in g <$> f a
-      Nil -> pure Nil
+      Tip k a   -> maybeSingleton k <$> f a
+      Nil       -> pure Nil
     in go
 
 instance FunctorWithIndex Word Patricia where
@@ -51,11 +106,8 @@ instance WitherableWithIndex Word Patricia where
   iwither f = let
     go t = case t of
       Bin p l r -> liftA2 (rebin p) (go l) (go r)
-      Tip k a -> let
-        g (Just b) = Tip k b
-        g Nothing  = Nil
-        in g <$> f k a
-      Nil -> pure Nil
+      Tip k a   -> maybeSingleton k <$> f k a
+      Nil       -> pure Nil
     in go
 
 instance Maplike Word Patricia where
@@ -66,70 +118,68 @@ instance Maplike Word Patricia where
 
   singleton = P.singleton
 
-{-
-{-# INLINE merge #-}
--- | \(\mathcal{O}(1)\texttt{+}, \mathcal{O}(n_A + n_B)\).
---   General merge of two trees.
---
---   Collision and single value functions __must__ return either
---   'Tip' with the respective key, or 'Nil'.
---
---   Subtree argument functions may return any tree, however the shape of said tree
---   __must__ be compatible with the prefix passed to the function.
---
---   This functions inlines when all argument functions are provided.
-merge
-  :: (Key -> a -> b -> Patricia c)                      -- ^ Collision
-  -> (Key -> a -> Patricia c)                           -- ^ Single left value
-  -> (Prefix -> Patricia a -> Patricia a -> Patricia c) -- ^ Left subtree
-  -> (Key -> b -> Patricia c)                           -- ^ Single right value
-  -> (Prefix -> Patricia b -> Patricia b -> Patricia c) -- ^ Right subtree
-  -> Patricia a
-  -> Patricia b
-  -> Patricia c
-merge (f :: Key -> x -> y -> Patricia c) oneX treeX oneY treeY = anyAny L
-  where
-    {-# INLINE side #-}
-    side one tree t =
+  alterF f !w = let
+    go t =
       case t of
-        Bin p l r -> tree p l r
-        Tip k a   -> one k a
-        Nil       -> Nil
+        Bin p l r
+          | P.beyond p w -> let
+              g (Just b) = join p t w (Tip w b)
+              g Nothing  = t
+              in g <$> f Nothing
+          | w < p      -> flip (rebinL p) r <$> go l
+          | otherwise  -> rebinR p l <$> go r
 
-    sideX = side oneX treeX
+        Tip k a
+          | k == w -> maybeSingleton k <$> f (Just a)
+          | otherwise -> let
+              g (Just b) = join k t w (Tip w b)
+              g Nothing  = t
+              in g <$> f Nothing
 
-    sideY = side oneY treeY
+        Nil -> maybeSingleton w <$> f Nothing
+    in go
 
-    sideA :: forall a b. S a b x y -> Patricia a -> Patricia c
+  alterMinWithKeyF f = let
+    go t = case t of
+      Bin p l r -> fmap (flip (rebinL p) r) <$> go l
+      Tip k a   -> Just (maybeSingleton k <$> f k a)
+      Nil       -> Nothing
+    in go
+
+  alterMaxWithKeyF f = let
+    go t = case t of
+      Bin p l r -> fmap (rebinR p l) <$> go r
+      Tip k a   -> Just (maybeSingleton k <$> f k a)
+      Nil       -> Nothing
+    in go
+
+  imergeA onL onR onB = let
+
+    sideX = runWhenMissing onL
+    sideY = runWhenMissing onR
+
     sideA s tA = case s of
                    L -> sideX tA
                    R -> sideY tA
 
-    sideB :: forall a b. S a b x y -> Patricia b -> Patricia c
     sideB s tB = case s of
                    L -> sideY tB
                    R -> sideX tB
 
-    anyAny
-      :: forall a b. S a b x y -> Patricia a -> Patricia b -> Patricia c
     anyAny s tA tB =
       case tA of
         Bin pA lA rA -> binAny s (# pA, lA, rA #) tA tB
-
         Tip kA a     -> tipAny s (# kA, a #) tA tB
-
         Nil          -> sideB s tB
 
-    tipAny
-      :: forall a b. S a b x y -> UTip a -> Patricia a -> Patricia b -> Patricia c
     tipAny s uA@(# kA, a #) tA tB =
       case tB of
         Bin pB lB rB -> tipBin s uA tA (# pB, lB, rB #)
 
         Tip kB b
           | kA == kB  -> case s of
-                           L -> f kA a b
-                           R -> f kA b a
+                           L -> matchedKey onB kA a b
+                           R -> matchedKey onB kA b a
 
           | otherwise -> case s of
                            L -> safeJoin kA (oneX kA a) kB (sideY tB)
@@ -137,8 +187,6 @@ merge (f :: Key -> x -> y -> Patricia c) oneX treeX oneY treeY = anyAny L
 
         Nil          -> sideA s tA
 
-    binAny
-      :: forall a b. S a b x y -> UBin a -> Patricia a -> Patricia b -> Patricia c
     binAny s uA tA tB =
       case tB of
         Bin pB lB rB -> binBin s uA tA (# pB, lB, rB #) tB
@@ -148,19 +196,15 @@ merge (f :: Key -> x -> y -> Patricia c) oneX treeX oneY treeY = anyAny L
 
         Nil          -> sideA s tA
 
-    tipBin
-      :: forall a b. S a b x y -> UTip a -> Patricia a -> UBin b -> Patricia c
     tipBin s uA@(# kA, a #) tA (# pB, lB, rB #)
-      | beyond pB kA = case s of
-                         L -> safeJoin kA (oneX kA a) pB (treeY pB lB rB)
-                         R -> safeJoin kA (oneY kA a) pB (treeX pB lB rB)
+      | P.beyond pB kA = case s of
+                           L -> safeJoin kA (oneX kA a) pB (treeY pB lB rB)
+                           R -> safeJoin kA (oneY kA a) pB (treeX pB lB rB)
 
       | kA < pB      = rebin pB (tipAny s uA tA lB) (sideB s rB)
 
       | otherwise    = rebin pB (sideB s lB) (tipAny s uA tA rB)
 
-    binBin
-      :: forall a b. S a b x y -> UBin a -> Patricia a -> UBin b -> Patricia b -> Patricia c
     binBin s uA@(# pA, lA, rA #) tA uB@(# pB, lB, rB #) tB =
       let {-# NOINLINE no #-}
           no = case s of
@@ -168,21 +212,22 @@ merge (f :: Key -> x -> y -> Patricia c) oneX treeX oneY treeY = anyAny L
                  R -> safeJoin pA (treeY pA lA rA) pB (treeX pB lB rB)
 
       in case Prelude.compare pA pB of
-           EQ                  -> rebin pA (anyAny s lA lB) (anyAny s rA rB)
+           EQ                    -> liftA2 (rebin pA) (anyAny s lA lB) (anyAny s rA rB)
 
-           LT | pB <= upper pA -> let !(# s' #) = other s
+           LT | pB <= P.upper pA -> let !(# s' #) = other s
 
-                                  in rebin pA (sideA s lA) (binAny s' uB tB rA)
+                                    in liftA2 (rebin pA) (sideA s lA) (binAny s' uB tB rA)
 
-              | pA >= lower pB -> rebin pB (binAny s uA tA lB) (sideB s rB)
-
-              | otherwise      -> no
-
-           GT | pA <= upper pB -> rebin pB (sideB s lB) (binAny s uA tA rB)
-
-              | pB >= lower pA -> let !(# s' #) = other s
-
-                                  in rebin pA (binAny s' uB tB lA) (sideA s rA)
+              | pA >= P.lower pB -> liftA2 (rebin pB) (binAny s uA tA lB) (sideB s rB)
 
               | otherwise      -> no
--}
+
+           GT | pA <= P.upper pB -> liftA2 (rebin pB) (sideB s lB) (binAny s uA tA rB)
+
+              | pB >= P.lower pA -> let !(# s' #) = other s
+
+                                    in liftA2 (rebin pA) (binAny s' uB tB lA) (sideA s rA)
+
+              | otherwise      -> no
+        
+    in anyAny L
