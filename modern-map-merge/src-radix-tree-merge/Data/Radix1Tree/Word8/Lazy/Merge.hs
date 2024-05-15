45f0ee55aab1393c0d89b31ba31a11db89ddfc85
James Cranch <cranch@cantab.net>
2024-05-15 10:56:33 +0100

    Work to do here

--- modern-map-merge/src-radix-tree-merge/Data/Radix1Tree/Word8/Lazy/Merge.hs
+++ modern-map-merge/src-radix-tree-merge/Data/Radix1Tree/Word8/Lazy/Merge.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE
+      MultiParamTypeClasses
   #-}
 
 
-module Data.Patricia.Word.Lazy.Merge where
+module Data.Radix1Tree.Word8.Lazy.Merge where
 
 import Data.Maplike
+import Data.Radix1Tree.Word8.Key (Build1)
+import Data.Radix1Tree.Word8.Lazy.Unsafe (Radix1Tree(..))
 
 
-instance Maplike Build1 Patricia where
+instance Maplike Build1 Radix1Tree where
